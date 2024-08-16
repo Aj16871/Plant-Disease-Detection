fbdff72a112808c02aa208f9ce718c2f0d7ceb9a
Aj16871 <aishwaryjoshi16@gmail.com>
2024-08-16 10:49:55 +0530

    Add files via upload

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import tensorflow as tf
 import numpy as np  # prints the current working directory
@@ -34,18 +35,6 @@ def get_class_name_from_predictions(predictions, filtered_class_names, class_nam
 class Precaution:
     def __init__(self):
         self.disease_details = {
-           'Apple___Apple_scab': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Venturia inaequalis*, which thrives in wet, cool conditions.\n",
-                'symptoms': "Olive-green to black velvety spots on leaves, fruits, and young twigs.\n",
-                'management': "Use resistant varieties, apply fungicides during the growing season, and remove fallen leaves.\n"
-            },
-            'Apple___Black_rot': {
-                'precaution': "Remove infected leaves, improve air circulation, and apply bactericides to prevent further spread.\n",
-                'cause': "Caused by the fungus *Botryosphaeria obtusa*, often entering through wounds or cracks in the bark.\n",
-                'symptoms': "Dark, sunken lesions on fruit, leaves, and bark, with fruit eventually rotting.\n",
-                'management': "Prune out infected branches, remove mummified fruits, and apply appropriate fungicides.\n"
-            },
             'Apple___Cedar_apple_rust': {
                 'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
                 'cause': "Caused by the fungus *Gymnosporangium juniperi-virginianae*, which requires both apple and cedar trees to complete its lifecycle.\n",
@@ -58,13 +47,7 @@ class Precaution:
                 'symptoms': "N/A\n",
                 'management': "Continue regular care to maintain plant health.\n"
             },
-            'Blueberry___healthy': {
-                'precaution': "No precautions needed, the plant is healthy.\n",
-                'cause': "N/A\n",
-                'symptoms': "N/A\n",
-                'management': "Continue regular care to maintain plant health.\n"
-            },
-            'Cherry_(including_sour)___Powdery_mildew': {
+                'Cherry_(including_sour)___Powdery_mildew': {
                 'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
                 'cause': "Caused by the fungus *Podosphaera clandestina*, which thrives in warm, dry conditions.\n",
                 'symptoms': "White, powdery fungal growth on leaves, shoots, and fruits.\n",
@@ -76,47 +59,25 @@ class Precaution:
                 'symptoms': "N/A\n",
                 'management': "Continue regular care to maintain plant health.\n"
             },
-            'Corn_(maize)___Cercospora_leaf_spot (Gray_leaf_spot)': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Cercospora zeae-maydis*, which thrives in warm, humid environments.\n",
-                'symptoms': "Small, rectangular lesions that eventually turn gray, leading to reduced photosynthesis.\n",
-                'management': "Rotate crops, till soil to bury crop residues, and apply fungicides if necessary.\n"
-            },
+            
             'Corn_(maize)___Common_rust': {
                 'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
                 'cause': "Caused by the fungus *Puccinia sorghi*, spread by wind-blown spores.\n",
                 'symptoms': "Reddish-brown pustules on both leaf surfaces, leading to leaf blighting.\n",
                 'management': "Use resistant corn varieties, apply fungicides if necessary, and practice crop rotation.\n"
             },
-            'Corn_(maize)___Northern_Leaf_Blight': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Exserohilum turcicum*, which thrives in humid conditions.\n",
-                'symptoms': "Large, cigar-shaped lesions on leaves, leading to premature leaf death.\n",
-                'management': "Use resistant hybrids, rotate crops, and apply fungicides during severe outbreaks.\n"
-            },
+            
             'Corn_(maize)___healthy': {
                 'precaution': "No precautions needed, the plant is healthy.\n",
                 'cause': "N/A\n",
                 'symptoms': "N/A\n",
                 'management': "Continue regular care to maintain plant health.\n"
             },
-            'Grape___Black_rot': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Guignardia bidwellii*, which overwinters in mummified berries and infected canes.\n",
-                'symptoms': "Brownish spots on leaves, black lesions on berries, and shriveling of fruit.\n",
-                'management': "Remove and destroy infected plant material, prune for good air circulation, and apply fungicides regularly.\n"
-            },
             'Grape___Esca_(Black_Measles)': {
                 'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
                 'cause': "Caused by a complex of fungi, including *Phaeomoniella chlamydospora* and *Phaeoacremonium aleophilum*.\n",
                 'symptoms': "Dark streaks in wood, leaf discoloration, and black spots on berries.\n",
                 'management': "Prune out infected wood, avoid excessive irrigation, and apply fungicides to reduce infection.\n"
-            },
-            'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Isariopsis clavispora*, which thrives in warm, wet conditions.\n",
-                'symptoms': "Irregular, dark brown spots on leaves that can lead to defoliation.\n",
-                'management': "Apply fungicides, remove and destroy affected leaves, and maintain good air circulation around vines.\n"
             },
             'Grape___healthy': {
                 'precaution': "No precautions needed, the plant is healthy.\n",
@@ -160,12 +121,6 @@ class Precaution:
                 'symptoms': "Dark brown spots with concentric rings on leaves, leading to defoliation.\n",
                 'management': "Use certified seed potatoes, rotate crops, and apply fungicides as needed.\n"
             },
-            'Potato___Late_blight': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the oomycete *Phytophthora infestans*, which thrives in wet, cool conditions.\n",
-                'symptoms': "Dark, water-soaked spots on leaves and stems, often with a pale green border, followed by rapid plant collapse.\n",
-                'management': "Use resistant varieties, avoid overhead watering, and destroy infected plants promptly.\n"
-            },
             'Potato___healthy': {
                 'precaution': "No precautions needed, the plant is healthy.\n",
                 'cause': "N/A\n",
@@ -202,47 +157,11 @@ class Precaution:
                 'symptoms': "N/A\n",
                 'management': "Continue regular care to maintain plant health."
             },
-            'Tomato___Bacterial_spot': {
-                'precaution': "Remove infected leaves, improve air circulation, and apply bactericides to prevent further spread.\n",
-                'cause': "Caused by several species of the bacterium *Xanthomonas*, spread by splashing water and contaminated tools.\n",
-                'symptoms': "Small, dark, water-soaked spots on leaves, stems, and fruit, often leading to defoliation.\n",
-                'management': "Practice crop rotation, avoid overhead irrigation, and use copper-based bactericides.\n"
-            },
             'Tomato___Early_blight': {
                 'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
                 'cause': "Caused by the fungus *Alternaria solani*, which thrives in warm, wet conditions.\n",
                 'symptoms': "Dark brown spots with concentric rings on leaves, leading to defoliation.\n",
                 'management': "Use certified seeds, rotate crops, and apply fungicides during wet weather.\n"
-            },
-            'Tomato___Late_blight': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the oomycete *Phytophthora infestans*, which thrives in wet, cool conditions.\n",
-                'symptoms': "Dark, water-soaked spots on leaves and stems, often with a pale green border, followed by rapid plant collapse.\n",
-                'management': "Use resistant varieties, avoid overhead watering, and destroy infected plants promptly.\n"
-            },
-            'Tomato___Leaf_Mold': {
-                'precaution': "Improve air circulation, remove infected leaves, and apply fungicides to prevent further spread.\n",
-                'cause': "Caused by the fungus *Passalora fulva*, which thrives in humid conditions.\n",
-                'symptoms': "Yellow spots on the upper leaf surface, with olive-green to gray mold on the underside.\n",
-                'management': "Reduce humidity, improve air circulation, and apply fungicides if necessary.\n"
-            },
-            'Tomato___Septoria_leaf_spot': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Septoria lycopersici*, which thrives in warm, wet conditions.\n",
-                'symptoms': "Small, water-soaked spots on leaves that turn into circular lesions with dark borders.\n",
-                'management': "Remove and destroy infected leaves, apply fungicides, and practice crop rotation.\n"
-            },
-            'Tomato___Spider_mites Two-spotted_spider_mite': {
-                'precaution': "Apply miticides and improve air circulation to prevent further spread.\n",
-                'cause': "Caused by the spider mite *Tetranychus urticae*, which feeds on plant cells.\n",
-                'symptoms': "Yellowing of leaves, with fine webbing on the undersides.\n",
-                'management': "Use miticides, increase humidity, and release natural predators like ladybugs or predatory mites.\n"
-            },
-            'Tomato___Target_Spot': {
-                'precaution': "Apply fungicides, remove infected leaves, and maintain good orchard hygiene to prevent further spread.\n",
-                'cause': "Caused by the fungus *Corynespora cassiicola*, which thrives in warm, humid conditions.\n",
-                'symptoms': "Small, dark lesions on leaves and fruit, often leading to defoliation and fruit rot.\n",
-                'management': "Apply fungicides, remove infected leaves, and improve air circulation around plants.\n"
             },
             'Tomato___Tomato_Yellow_Leaf_Curl_Virus': {
                 'precaution': "Control whitefly population and remove infected plants to prevent further spread.\n",
@@ -283,7 +202,7 @@ def display_prediction(class_name):
     precautions = precaution.get_precaution(class_name)
     st.warning(f"{precautions}")
 
-# Sidebar for navigation
+# Sidebar for  navigation
 st.sidebar.title("🌱 Dashboard")
 app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recognition", "Webcam Live Feed", "Disease Library"])
 
@@ -463,19 +382,18 @@ elif app_mode == "Disease Recognition":
 
                 # Full list of class names
                 class_names = [
-                    'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
-                    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew',
+                    'Apple___Cedar_apple_rust', 'Apple___healthy',
+                    'Cherry_(including_sour)___Powdery_mildew',
                     'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot (Gray_leaf_spot)',
                     'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy',
-                    'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                    'Grape___Esca_(Black_Measles)', 
                     'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
                     'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy',
-                    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
-                    'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew',
-                    'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot',
-                    'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold',
-                    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite',
-                    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                    'Potato___Early_blight', 'Potato___healthy',
+                    'Squash___Powdery_mildew',
+                    'Strawberry___Leaf_scorch', 'Strawberry___healthy',
+                    'Tomato___Early_blight',                 
+                    'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
                     'Tomato___healthy'
                 ]
                 
@@ -512,20 +430,19 @@ elif app_mode == "Webcam Live Feed":
 
                     # Full list of class names
                     class_names = [
-                        'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
-                        'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew',
-                        'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot (Gray_leaf_spot)',
-                        'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy',
-                        'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-                        'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
-                        'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy',
-                        'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
-                        'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew',
-                        'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot',
-                        'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold',
-                        'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite',
-                        'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
-                        'Tomato___healthy'
+                    'Apple___Cedar_apple_rust', 'Apple___healthy',
+                    'Cherry_(including_sour)___Powdery_mildew',
+                    'Cherry_(including_sour)___healthy',
+                    'Corn_(maize)___Common_rust_', 'Corn_(maize)___healthy',
+                    'Grape___Esca_(Black_Measles)', 
+                    'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+                    'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy',
+                    'Potato___Early_blight', 'Potato___healthy',
+                    'Squash___Powdery_mildew',
+                    'Strawberry___Leaf_scorch', 'Strawberry___healthy',
+                    'Tomato___Early_blight',                 
+                    'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                    'Tomato___healthy'
                     ]
                     
                     filtered_class_names = filter_class_names(plant_prefix, class_names)
